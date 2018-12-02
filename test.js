8c74cc059df0b42c8dc79e595e76fa9349675680
Kartik Javali <40225782+pkjavali@users.noreply.github.com>
2018-12-02 16:59:45 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,9 @@
-
-
 var xdrequest = new XDomainRequest();
-
 xdrequest.onload = function(){
-
-var jsresponse = JSON.parse(request.responseText);
-console.log(jsresponse.lp1);
-document.getElementById('s1').innerHTML = "name";
-document.getElementById('val1').innerHTML = "Val";
-
-}
+  var jsresponse = JSON.parse(request.responseText);
+  console.log(jsresponse.lp1);
+  document.getElementById('s1').innerHTML = "name";
+  document.getElementById('val1').innerHTML = "Val";
+  }
 xdrequest.open('GET', 'https://console.thethingsnetwork.org/applications/kerlink-test-app-01/integrations/http-ttn/kerlink-httptest-01',true);
-
 xdrequest.send();
