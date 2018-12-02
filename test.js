c3a1355871ec0547f1b3d08ae0bed5360493839a
Kartik Javali <40225782+pkjavali@users.noreply.github.com>
2018-12-02 15:55:08 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function(){
+if(this.readyState==4 && this.status == 200){
+var jsresponse = JSON.parse(request.responseText);
+console.log(jsresponse.lp1);
 document.getElementById('s1').innerHTML = "name";
 document.getElementById('val1').innerHTML = "Val";
+}
+}
+request.open('GET', 'https://console.thethingsnetwork.org/applications/kerlink-test-app-01/devices/mcf-lw06485/data',true);
+request.send();
